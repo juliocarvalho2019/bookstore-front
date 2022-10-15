65ec283de8e128af83d553aa47225eb45517af39
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-15 10:20:54 -0300

    Feat criado html e css para componente categoria-create.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,9 @@ import {MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
-
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -42,7 +44,10 @@ import { CategoriaCreateComponent } from './components/views/categoria/categoria
     MatCardModule,
     MatTableModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule
 
   ],
   providers: [],
