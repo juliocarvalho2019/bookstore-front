1c52ea8ed78cefa364c7851d4e3726f89c1cb02a
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-16 09:30:27 -0300

    Feat: feito html e css categoria-delete.

--- src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
+++ src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categoria-delete',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CategoriaService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  cancel(): void {
+    this.router.navigate(['categorias']);
   }
 
 }
