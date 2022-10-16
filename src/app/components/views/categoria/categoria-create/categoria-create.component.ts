7d56027c28cb0773eefe581190501c9d758b3678
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-16 08:58:31 -0300

    Feat: Feito validacoes para os campos do create.

--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -24,6 +24,10 @@ export class CategoriaCreateComponent implements OnInit {
   this.service.create(this.categoria).subscribe((resposta) => {
     this.router.navigate(['categorias'])
     this.service.mensagem('Categoria criada com sucesso!');
+  }, err => {
+    for(let i = 0; i < err.error.errors.length; i++){
+      this.service.mensagem(err.error.errors[i].message)
+    }
   })
 }
 
