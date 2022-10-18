05f40fa0ea6551118aa50774d592e7e62bded6c6
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-18 20:55:44 -0300

    Feat: feito leitura read all livros.

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
+import { LivroService } from './../livro.service';
+import { Livro } from './../livro.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 export class LivroReadAllComponent implements OnInit {
  
   displayedColumns: string[] = ['id', 'titulo', 'livros', 'acoes'];
-  constructor() { }
+
+  id_cat : String = "";
+  livros: Livro[] = [];
+
+  constructor(private service: LivroService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAllByCategoria(this.id_cat).subscribe((resposta)=> {
+      this.livros = resposta;  
+    })
   }
 
 }
