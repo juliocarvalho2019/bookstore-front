4d63f5228b6a3496f2131ba470b7e2a913046801
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-18 20:23:19 -0300

    Feat: criado componente read-livro-all e feito rota.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
@@ -29,6 +30,11 @@ const routes: Routes = [
     path:'categorias/update/:id',
 
        component: CategoriaUpdateComponent
+  },
+  {
+    path:'categorias/:id_cat/livros',
+
+       component: LivroReadAllComponent
   },
 
 ];
