49f19f80130d874d5ab6288184bb9ed999937766
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-23 12:04:07 -0300

    Feat: ajustado html e css update livros.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
@@ -42,6 +43,12 @@ const routes: Routes = [
 
        component: LivroCreateComponent
   },
+  {
+    path:'categorias/:id_cat/livros/:id/update',
+
+       component: LivroUpdateComponent
+  },
+
 
 
 ];
