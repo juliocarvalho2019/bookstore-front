2b2b51924567ad017f30dde502f491f1cbea22c6
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-23 11:50:46 -0300

    Feat: feito livro create.

--- src/app/components/views/livro/livro-create/livro-create.component.ts
+++ src/app/components/views/livro/livro-create/livro-create.component.ts
@@ -1,5 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { LivroService } from '../livro.service';
+import { Livro } from '../livro.model';
 
 @Component({
   selector: 'app-livro-create',
@@ -8,14 +11,34 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LivroCreateComponent implements OnInit {
 
+  id_cat: String = ''
+
+  livro: Livro = {
+    id:'',
+    titulo:'',
+    nome_autor:'',
+    texto:''
+  }
+
   titulo = new FormControl("", [Validators.minLength(3)]);
   nome_autor = new FormControl("", [Validators.minLength(3)]);
   texto = new FormControl("", [Validators.minLength(10)]);
 
 
-  constructor() { }
+  constructor( private service: LivroService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+  }
+
+  create(): void {
+    this.service.create(this.livro, this.id_cat).subscribe(resposta => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem("Livro criado com sucesso!");
+  }, err => {
+    this.router.navigate([`categorias;${this.id_cat}/livros`]);
+    this.service.mensagem("Erro ao criar novo livro, tente mais tarde!");
+  })
   }
 
   getMessage(){
@@ -29,6 +52,10 @@ export class LivroCreateComponent implements OnInit {
       return "O campo TEXTO deve conter entre 10 e 1000000 caracteres";
     }
     return false;
+  }
+
+  cancel(): void {
+    this.router.navigate(['livros']);
   }
 
 }
--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { Livro } from './livro.model';
 import { HttpClient } from '@angular/common/http';
@@ -11,10 +12,24 @@ export class LivroService {
 
   baseUrl: String = environment.baseUrl
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAllByCategoria(id_cat: String):Observable <Livro[]>{
     const url = `${this.baseUrl}/livros?categoria=${id_cat}`
     return this.http.get<Livro[]>(url)
   }
+
+  create(livro: Livro, id_cat: String):Observable <Livro>{
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    return this.http.post<Livro>(url, livro)
+  }
+
+  mensagem(str: String): void{
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000    
+    })
+  }
+
 }
