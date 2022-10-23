3139436f5562e44846bd7cc22cd110226e79264c
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-10-23 11:20:11 -0300

    Feat: feito validações dos campos para criar livro.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
@@ -57,7 +57,8 @@ import { LivroCreateComponent } from './components/views/livro/livro-create/livr
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
